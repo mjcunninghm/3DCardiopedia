1f92412012e4b94fcd2ca4440327f81ff349e305
mjcunninghm <89094719+mjcunninghm@users.noreply.github.com>
2023-02-14 21:47:03 -0600

    Added icons to the tab navigator

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './components/HomeScreen.js';
 import PatientPortal from './components/PatientPortal/PatientPortal.js';
-import CareTeamPortal from './components/CareTeamPortal.js'
-import Library from './components/Library.js'
+import CareTeamPortal from './components/CareTeamPortal.js';
+import Library from './components/Library.js';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName={'HomeScreen'}>
-        <Tab.Screen name="HomeScreen" component={HomeScreen} />
-        <Tab.Screen name="PatientPortal" component={PatientPortal} />
-        <Tab.Screen name="CareTeamPortal" component={CareTeamPortal} />
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Patient Portal') {
+              iconName = focused ? 'person' : 'person-outline';
+            } else if (route.name === 'Care Team Portal') {
+              iconName = focused ? 'medkit' : 'medkit-outline';
+            } else if (route.name === 'Library') {
+              iconName = focused ? 'library' : 'library-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#2227ff',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Patient Portal" component={PatientPortal} />
+        <Tab.Screen name="Care Team Portal" component={CareTeamPortal} />
         <Tab.Screen name="Library" component={Library} />
       </Tab.Navigator>
     </NavigationContainer>
