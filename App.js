f2b942a3794c3d27bbb2718bdff48a52edca3a6a
mjcunninghm <89094719+mjcunninghm@users.noreply.github.com>
2023-02-23 18:56:04 -0600

    Restructured navigation

--- App.js
+++ App.js
@@ -1,68 +1,89 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './components/HomeScreen.js';
-import PatientPortal from './components/PatientPortal/PatientPortal.js';
+import PatientPortal from './components/PatientPortal.js';
 import CareTeamPortal from './components/CareTeamPortal.js';
 import Library from './components/Library.js';
+import ModelScreen from './components/ModelScreen';
 import ModelView from './components/view3D';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Patient Portal') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'Care Team Portal') {
+            iconName = focused ? 'medkit' : 'medkit-outline';
+          } else if (route.name === 'Library') {
+            iconName = focused ? 'library' : 'library-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#2227ff',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Patient Portal"
+        component={PatientPortal}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Care Team Portal"
+        component={CareTeamPortal}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Library"
+        component={Library}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Patient Portal') {
-              iconName = focused ? 'person' : 'person-outline';
-            } else if (route.name === 'Care Team Portal') {
-              iconName = focused ? 'medkit' : 'medkit-outline';
-            } else if (route.name === 'Library') {
-              iconName = focused ? 'library' : 'library-outline';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: '#2227ff',
-          tabBarInactiveTintColor: 'gray',
-        })}>
-        <Tab.Screen
+      <Stack.Navigator>
+        <Stack.Screen
           name="Home"
-          component={HomeScreen}
+          component={Tabs}
           options={{
             headerShown: false,
           }}
         />
-        <Tab.Screen
-          name="Patient Portal"
-          component={PatientPortal}
-          options={{
-            headerShown: false,
-          }}
+        <Stack.Screen
+          name="Model Screen"
+          component={ModelScreen}
         />
-        <Tab.Screen
-          name="Care Team Portal"
-          component={CareTeamPortal}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen
-          name="Library"
-          component={Library}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
